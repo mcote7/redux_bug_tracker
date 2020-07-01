c573575bd9c7b64509dbe0fecb674c1e23c93365
Michael Cote <michaelcote7@outlook.com>
2020-06-30 18:30:38 -0700

    .

--- src/actions.js
+++ src/actions.js
@@ -3,6 +3,13 @@ import * as actions from './actionTypes';
 export const bugAdded = description => ({
   type: actions.BUG_ADDED, 
   payload: {
-    description: "Bug1"
+    description
+  }
+});
+
+export const bugResolved = id => ({
+  type: actions.BUG_RESOLVED,
+  payload: {
+    id
   }
 })
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import store from './store';
-import {bugAdded} from './actions';
+import {bugAdded, bugResolved} from './actions';
 
 store.dispatch(bugAdded("Bug1"));
+store.dispatch(bugResolved(1));
 
 console.log("store:",store.getState());
