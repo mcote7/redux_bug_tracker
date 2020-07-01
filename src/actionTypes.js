6730552bb1b264f053245616651fd2e54979aacb
Michael Cote <michaelcote7@outlook.com>
2020-06-30 17:38:19 -0700

    .

---
+++ src/actionTypes.js
@@ -0,0 +1,3 @@
+export const BUG_ADDED = "bugAdded";
+export const BUG_REMOVED = "bugRemoved";
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import store from './store';
+import * as actions from './actionTypes';
+
+const unsubscribe = store.subscribe(() => {
+  console.log("store changed", store.getState())
+})
 
 store.dispatch({
-  type: "bugAdded",
+  type: actions.BUG_ADDED,
   payload: {
     description: "Bug1"
   }
 });
+unsubscribe();
 store.dispatch({
-  type: "bugRemoved",
+  type: actions.BUG_REMOVED,
   payload: {
     id: 1
   }
