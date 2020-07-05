5595f3066ebe1332cce365a7f3837530cf2a16a3
Michael Cote <michaelcote7@outlook.com>
2020-07-05 13:53:34 -0700

    .

--- redux/src/store/middleware/logger.js
+++ redux/src/store/middleware/logger.js
@@ -1,5 +1,8 @@
 const logger = param => store => next => action => {
-  console.log("Logging", param);
+  console.log("next", next);
+  console.log("Store", store);
+  console.log("action", action);
+  console.log("param", param);
   next(action);
 };
 export default logger;
